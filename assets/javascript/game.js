e9eb4ca682d7709e1aa888d22723987ee500c527
WesPres1990 <lawpres@gmail.com>
2017-12-17 06:51:34 -0500

    Second Upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,11 @@
-var crystalNo1= Math.floor(Math.random()*11+1)
-var crystalNo2= Math.floor(Math.random()*11+1)
-var crystalNo3= Math.floor(Math.random()*11+1)
-var crystalNo4= Math.floor(Math.random()*11+1)
+var crystalNo1= Math.floor((Math.random() * 12) + 1);
+var crystalNo2= Math.floor((Math.random() * 12) + 1);
+var crystalNo3= Math.floor((Math.random() * 12) + 1);
+var crystalNo4= Math.floor((Math.random() * 12) + 1);
 
-var randomGameNumber=Math.floor(Math.random()*101+19)
+var randomGameNumber=Math.floor((Math.random() * 120) + 19);
+
+$('#randomGameNumber').text(randomGameNumber);
  
 var wins= 0;
 var losses = 0;
@@ -12,43 +14,54 @@ var score= 0;
 
 
 function reset(){
-    console.log(randomGameNumber)
+    randomGameNumber=Math.floor((Math.random() * 120) + 19);
+    console.log(randomGameNumber);
     $('#randomGameNumber').text(randomGameNumber);
-    crystalNo1;
-    crystalNo2;
-    crystalNo3;
-    crystalNo4;
-    score;
-    $('#score').text(score);
+    crystalNo1= Math.floor((Math.random() * 12) + 1);
+    crystalNo2= Math.floor((Math.random() * 12) + 1);
+    crystalNo3= Math.floor((Math.random() * 12) + 1);
+    crystalNo4= Math.floor((Math.random() * 12) + 1);
+    score=0;
+    $('#finalScore').text(score);
 }; 
 
 var logic = function (){
 	if (score == randomGameNumber) {
 		wins = wins + 1;
-		$('#wins').text(wins);
+        $('#wins').text(wins);
+        alert("WIN!");
 		reset();
 	}else if(score > randomGameNumber){
 		losses = losses + 1;
-		$('#loss').text(losses);
+        $('#loss').text(losses);
+        alert("LOSE!!");
         reset();
     }
 };
 
 $(document).ready(function(){
 	$('#Crystal_cosmic').click(function(){
-		score = score + crystalNo1;
+        score = score + crystalNo1;
+        console.log("New score= " + score);
+        $('#finalScore').text(score);
 		logic();
 	})
 	$('#Crystal_lottery').click(function(){
-		score = score + crystalNo2;
+        score = score + crystalNo2;
+        console.log("New score= " + score);
+        $('#finalScore').text(score);
 		logic();
 	})
 	$('#Crystal_quest').click(function(){
-		score = score + crystalNo3;
+        score = score + crystalNo3;
+        console.log("New score= " + score);
+        $('#finalScore').text(score);
 		logic();
 	})
 	$('#Crystal_rare').click(function(){
-		score = score + crystalNo4;
+        score = score + crystalNo4;
+        console.log("New score= " + score);
+        $('#finalScore').text(score);
 		logic();
 	})
 });
