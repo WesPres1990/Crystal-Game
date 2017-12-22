c4f6f6580c402d5e20930cf58c2fb2f57dc2fa84
WesPres1990 <lawpres@gmail.com>
2017-12-22 02:37:09 -0500

    Add-Info-and-Update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
 //* Variables *//
 
 //* Crystal random number viables *//
-var crystalNo1= Math.floor((Math.random() * 12) + 1);
-var crystalNo2= Math.floor((Math.random() * 12) + 1);
-var crystalNo3= Math.floor((Math.random() * 12) + 1);
-var crystalNo4= Math.floor((Math.random() * 12) + 1);
+var crystalNo1 = Math.floor((Math.random() * 12) + 1);
+var crystalNo2 = Math.floor((Math.random() * 12) + 1);
+var crystalNo3 = Math.floor((Math.random() * 12) + 1);
+var crystalNo4 = Math.floor((Math.random() * 12) + 1);
 
 //* Target winnning game number viable and identifying its operation *//
-var randomGameNumber=Math.floor((Math.random() * 120) + 19);
+var randomGameNumber = Math.floor((Math.random() * 120) + 19);
 
 $('#randomGameNumber').text(randomGameNumber);
 
@@ -16,31 +16,31 @@ var images = ["assets/images/Crystal_cosmic.png", "assets/images/Crystal_lottery
  
 
 //* Variables for wins, losses, and score *//
-var wins= 0;
+var wins = 0;
 var losses = 0;
-var score= 0;
+var score = 0;
 
 //* Game Play *//
 
 //* Resetting the game *//
 function reset(){
-    randomGameNumber=Math.floor((Math.random() * 120) + 19);
+    randomGameNumber = Math.floor((Math.random() * 120) + 19);
     console.log(randomGameNumber);
     $('#randomGameNumber').text(randomGameNumber);
-    crystalNo1= Math.floor((Math.random() * 12) + 1);
-    crystalNo2= Math.floor((Math.random() * 12) + 1);
-    crystalNo3= Math.floor((Math.random() * 12) + 1);
-    crystalNo4= Math.floor((Math.random() * 12) + 1);
-    score=0;
+    crystalNo1 = Math.floor((Math.random() * 12) + 1);
+    crystalNo2 = Math.floor((Math.random() * 12) + 1);
+    crystalNo3 = Math.floor((Math.random() * 12) + 1);
+    crystalNo4 = Math.floor((Math.random() * 12) + 1);
+    score = 0;
     $('#finalScore').text(score);
 }; 
 
-//* Logic for conditions concerning wins, losses, and resets *//
-var logic = function (){
+//* Logic for conditions concerning scoring, wins, losses, and resets *//
+var result = function (){
 	if (score == randomGameNumber) {
 		wins = wins + 1;
         $('#wins').text(wins);
-        alert("WIN!");
+        alert("WIN!!");
         reset();
         
 	}else if(score > randomGameNumber){
@@ -55,29 +55,29 @@ var logic = function (){
 $(document).ready(function(){
 	$('#Crystal_cosmic').click(function(){
         score = score + crystalNo1;
-        console.log("New score= " + score);
+        console.log("Latest score= " + score);
         $('#finalScore').text(score);
-		logic();
+		result();
     })
     
 	$('#Crystal_lottery').click(function(){
         score = score + crystalNo2;
-        console.log("New score= " + score);
+        console.log("Latest score= " + score);
         $('#finalScore').text(score);
-		logic();
+		result();
     })
     
 	$('#Crystal_quest').click(function(){
         score = score + crystalNo3;
-        console.log("New score= " + score);
+        console.log("Latest score= " + score);
         $('#finalScore').text(score);
-		logic();
+		result();
     })
     
 	$('#Crystal_rare').click(function(){
         score = score + crystalNo4;
-        console.log("New score= " + score);
+        console.log("Latest score= " + score);
         $('#finalScore').text(score);
-		logic();
+		result();
 	})
 });
